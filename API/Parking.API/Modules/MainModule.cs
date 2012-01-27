7a6a54d4f165677ba8099e33766356e5781127ab
Daniel Ramirez <daniel.rmz@gmail.com>
2012-01-26 18:16:16 -0600

    Added API and UI modules

---
+++ API/Parking.API/Modules/MainModule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using Nancy;
+using Nancy.ViewEngines.Razor;
+
+namespace Parking.API.Modules
+{
+    public class MainModule : NancyModule
+    {
+        public MainModule()
+        {
+            Get["/"] = parameters =>
+            {
+                return "Hello World";
+            };
+        }
+    }
+}
